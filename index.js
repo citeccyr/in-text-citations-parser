badabf594febc380b5d91a584c08706129683f4d
Mrau Hu <mrauhu@yandex.ru>
2017-03-27 03:25:00 +0300

    Updated index.js, added `XMLTransform` and `FileTransform` exports.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 module.exports.ParserTransform = require('./lib/ParserTransform');
+module.exports.XMLTransform = require('./lib/XMLTransform');
+module.exports.FileTransform = require('./lib/FileTransform');
