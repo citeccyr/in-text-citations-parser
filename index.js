216e2f005adf5eebcc266b428eca64129b3097e3
Mrau Hu <mrauhu@yandex.ru>
2017-04-17 00:57:26 +0300

    Added class `XMLinTextRefTransform` with `test_in_text_ref.js`

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.ParserTransform = require('./lib/ParserTransform');
 module.exports.XMLTransform = require('./lib/XMLTransform');
+module.exports.XMLinTextRefTransform = require('./lib/XMLinTextRefTransform');
 module.exports.FileTransform = require('./lib/FileTransform');
